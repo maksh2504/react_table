8f58c7eec9ba4f8b0f1ea4393982bb35e805f2c6
maksh2504 <maksh2501@gmail.com>
2022-10-24 14:06:19 +0300

    git commit

--- src/store/actions/usersAction.ts
+++ src/store/actions/usersAction.ts
@@ -6,9 +6,8 @@ export const getUsersAction = () => {
     return async (dispatch: Dispatch<UsersAction>) => {
         try {
             dispatch({type: UsersActionTypes.GET_USERS})
-            await getUsers().then(
-                users => dispatch({type: UsersActionTypes.GET_USERS_SUCCESS, payload: users})
-            )
+            const users = await getUsers()
+            dispatch({type: UsersActionTypes.GET_USERS_SUCCESS, payload: users})
         } catch (e) {
             dispatch({type: UsersActionTypes.GET_USERS_ERROR, payload: 'ERROR'})
         }
