298f90bc350ebfb66ab984cd516fb9e40de9a5c2
maksh2504 <maksh2501@gmail.com>
2022-10-26 10:35:25 +0300

    git commit

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import Header from "./components/Header";
 import Row from "./components/Row";
 import './Table.css';
