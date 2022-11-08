aad9c9a36ac67660ecc423d22966c8a56afe73f2
maksh2504 <maksh2501@gmail.com>
2022-11-08 13:14:38 +0300

    git commit

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,31 +2,15 @@ import React, {useState} from 'react';
 import './HomePage.css'
 import {useDispatch} from "react-redux";
 import {name} from "../../store/slices/name";
+import EnterName from "../../components/EnterName";
 
 const HomePage = () => {
-    const [localName, setLocalName] = useState('')
-    const dispatch = useDispatch()
-    const {newName} = name.actions
-
-    const changeStateName = () => {
-        dispatch(newName(localName))
-        setLocalName('');
-    }
-
     return (
         <div>
             <h1 className='label'>
                 Home page.
             </h1>
-            <div className='inputBox'>
-                <div>
-                    Enter your name.
-                </div>
-                <input type='text' value={localName} onChange={e => setLocalName(e.target.value)}/>
-                <button onClick={() => {changeStateName()}}>
-                    Save
-                </button>
-            </div>
+            <EnterName/>
         </div>
     );
 };
